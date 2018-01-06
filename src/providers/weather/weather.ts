887f0a3d47d19f935209c1eec65c5ca372ca8d14
Gaurav Mehla <gaurav.ror007@gmail.com>
2018-01-06 23:20:38 +0530

    iWeather Basic MVC Done

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,20 @@
+import { Http, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WeatherProvider {
+	apiKey = '6e3b1b1215c743e4' ;
+	url;
+
+  	constructor(public http: Http) {
+    	console.log('Hello WeatherProvider Provider');
+    	this.url = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q/';
+  	}
+
+  	getWeather( city, state ) {
+  		return this.http.get( this.url + state + '/' + city + '.json')
+  			.map( res => res.json() );
+  	}
+
+}
